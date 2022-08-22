1e0f192fcff85ccb49e2340eeefefdc33db2de1c
Kenny Tran <97045625+mrkennytran@users.noreply.github.com>
2022-08-21 23:45:58 -0500

    Delete includes directory

--- includes/README.md
+++
@@ -1,2 +0,0 @@
-# phone-book
-A simple web application for saving someone's contact number (php,mysql,html,css, agile)
--- includes/logout.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-session_unset(); //session var created and delete val in it 
-session_destroy(); //
-header("Location: ../index.php");
